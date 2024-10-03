c1f3b47537f8fb70482266299d55209b883a3e6f
Ojasvi Tomar <68610852+ojasvi913@users.noreply.github.com>
2024-10-03 19:42:11 +0530

    task1a and 1b

---
+++ src/build/ur_simulation_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ojasvi/colcon_ws2/src/install/ur_description/share/ur_description/package.sh"
+. "/home/ojasvi/colcon_ws2/src/install/ur_moveit_config/share/ur_moveit_config/package.sh"
